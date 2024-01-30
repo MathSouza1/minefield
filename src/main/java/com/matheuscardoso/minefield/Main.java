d508ddb7ffa711a3a402b6d34f09c9a7834416f9
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-01-30 10:55:52 -0300

    Implementing Main.java

--- src/main/java/com/matheuscardoso/minefield/Main.java
+++ src/main/java/com/matheuscardoso/minefield/Main.java
@@ -1,6 +1,11 @@
 package com.matheuscardoso.minefield;
 
+import com.matheuscardoso.minefield.model.Board;
+import com.matheuscardoso.minefield.view.BoardConsole;
+
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board(6,6,3);
+        new BoardConsole(board);
     }
 }
