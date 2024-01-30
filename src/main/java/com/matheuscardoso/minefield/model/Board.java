bd463b43afe2f5c64264805a0286d6b4c37c294f
Matheus Cardoso <eunaogostodetrabalhos123@gmail.com>
2024-01-30 10:54:45 -0300

    Adding new functionalities to Board.java and implementing unit tests

--- src/main/java/com/matheuscardoso/minefield/model/Board.java
+++ src/main/java/com/matheuscardoso/minefield/model/Board.java
@@ -1,5 +1,7 @@
 package com.matheuscardoso.minefield.model;
 
+import com.matheuscardoso.minefield.exceptions.ExplosionException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -43,5 +45,60 @@ public class Board {
                 fields.add(new Field(row, column));
             }
         }
+    }
+
+    public boolean goalAchieved() {
+        return fields.stream().allMatch(Field::goalAchieved);
+    }
+
+    public void restart() {
+        fields.forEach(Field::restart);
+        drawMines();
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("  ");
+        for (int column = 0; column < columns; column++) {
+            stringBuilder.append(" ");
+            stringBuilder.append(column);
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append("\n");
+
+        int i = 0;
+        for (int row = 0; row < rows; row++) {
+            stringBuilder.append(row);
+            stringBuilder.append(" ");
+            for (int column = 0; column < columns; column++) {
+                stringBuilder.append(" ");
+                stringBuilder.append(fields.get(i));
+                stringBuilder.append(" ");
+                i++;
+            }
+            stringBuilder.append("\n");
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public void open(Integer row, Integer column) {
+        try {
+            fields.parallelStream()
+                    .filter(field -> field.getRow() == row && field.getColumn() == column)
+                    .findFirst()
+                    .ifPresent(field -> field.openField());
+        } catch (ExplosionException e) {
+            fields.forEach(field -> field.setOpen(true));
+            throw e;
+        }
+    }
+
+    public void toggleMarking(Integer row, Integer column) {
+        fields.parallelStream()
+                .filter(field -> field.getRow() == row && field.getColumn() == column)
+                .findFirst()
+                .ifPresent(field -> field.changeFlag());
     }
 }
--- src/test/java/com/matheuscardoso/minefield/model/BoardTest.java
+++ src/test/java/com/matheuscardoso/minefield/model/BoardTest.java
@@ -3,17 +3,77 @@ package com.matheuscardoso.minefield.model;
 import com.matheuscardoso.minefield.exceptions.ExplosionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BoardTest {
 
+    @InjectMocks
+    private Board board;
+
+    @Mock
+    private Field field;
+
     @BeforeEach
     void initBoard() {
-        Board board = new Board(3, 3, 2);
+        board = new Board(3, 3, 2);
     }
 
     @Test
-    void runBoardSuccessfully() {
+    void testGoalAchieved() {
+        boolean valid = true;
+        try {
+            board.goalAchieved();
+        } catch (Exception e) {
+            valid = false;
+        }
+        assertTrue(valid);
+    }
+
+    @Test
+    void testRestart() {
+        boolean valid = true;
+        try {
+            board.restart();
+        } catch (Exception e) {
+            valid = false;
+        }
+        assertTrue(valid);
+    }
+
+    @Test
+    void testToString() {
+        boolean valid = true;
+        try {
+            board.toString();
+        } catch (Exception e) {
+            valid = false;
+        }
+        assertTrue(valid);
+    }
+
+    @Test
+    void testOpenSuccessfully() {
+        boolean valid = true;
+        try {
+            board.open(3,3);
+        } catch (Exception e) {
+            valid = false;
+        }
+        assertTrue(valid);
+    }
+
+    @Test
+    void testToggleMarking() {
+        boolean valid = true;
+        try {
+            board.toggleMarking(3,3);
+        } catch (Exception e) {
+            valid = false;
+        }
+        assertTrue(valid);
     }
 }
